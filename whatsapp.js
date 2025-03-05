e708dba4f795cbec69c42562493238e487084e08
erfanhuda <erfanhuda@DESKTOP-T470s-ERFANHUDA>
2025-03-05 22:46:42 +0700

    update 5xY

--- whatsapp.js
+++ whatsapp.js
@@ -6,11 +6,7 @@ function sendMessage() {
     let textMessage = document.getElementById('description').value;
 
 
-    var url = "https://wa.me/" + number + "?text="
-    + "Name : " + nameFrom + "%0a" 
-    + "Email : " + emailFrom + "%0a"
-    + "Message : " + textMessage + "%0a%0a";
+    var url = "https://wa.me/" + number + "?text=" + "Name : " + nameFrom + "%0a"  + "Email : " + emailFrom + "%0a" + "Message : " + textMessage + "%0a%0a";
 
     window.open(url, '_blank').focus();
-
 }
