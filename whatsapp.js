bdb178cd74c0062145a7cf933b8088dd3adc0c31
erfanhuda <erfanhuda@DESKTOP-T470s-ERFANHUDA>
2025-03-05 22:24:30 +0700

    edit

---
+++ whatsapp.js
@@ -0,0 +1,17 @@
+let sendMessage = () => {
+    let number = "+6285799663331";
+    
+    let mainEl = document.querySelector('#contact-form');
+    let emailFrom = mainEl.getElementById('email').value;
+    let nameFrom = mainEl.getElementById('name').value;
+    let textMessage = mainEl.getElementById('description').value;
+
+
+    var url = "https://wa.me/" + number + "?text="
+    + "Name : " + nameFrom + "%0a" 
+    + "Email : " + emailFrom + "%0a"
+    + "Message : " + textMessage + "%0a%0a";
+
+    window.open(url, '_blank').focus();
+
+}
