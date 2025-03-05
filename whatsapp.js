5ae19961ff5bda098d9fa9ac02bd7314c856e658
erfanhuda <erfanhuda@DESKTOP-T470s-ERFANHUDA>
2025-03-05 22:56:09 +0700

    YYYY

--- whatsapp.js
+++ whatsapp.js
@@ -2,7 +2,7 @@ function sendMessage() {
     let number = "+6285799663331";
     
     let emailFrom = document.getElementById('email').value;
-    let nameFrom = document.getElementById('name').value;
+    let nameFrom = document.getElementById('first-name').value;
     let textMessage = document.getElementById('description').value;
 
 
